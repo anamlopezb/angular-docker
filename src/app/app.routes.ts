f75c08eac93e946dc5b75cd6d9c7dca53f22ecd3
anamlopezb <80725577+anamlopezb@users.noreply.github.com>
2024-11-01 15:51:59 -0500

    Miguel agendamientos (#1)
    * postulaciones
    
    * encuentros
    
    * agendamientos
    
    ---------
    
    Co-authored-by: MMMiguel2103 <yeiden2103@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,23 @@
 import { Routes } from '@angular/router';
 import { PracticesComponent } from './practices/practices.component';
+import { PostulacionesComponent } from './postulaciones/postulaciones.component';
+import { EncuentrosComponent } from './encuentros/encuentros.component';
+import { AgendamientosComponent } from './agendamientos/agendamientos.component';
 
 export const routes: Routes = [
   { path: 'practices', component: PracticesComponent },
+  {
+    path: 'postulaciones',
+    component: PostulacionesComponent
+},
+{
+    path: 'encuentros',
+    component: EncuentrosComponent
+},
+
+{
+  path: 'agendamientos',
+  component: AgendamientosComponent
+}
+
 ];
